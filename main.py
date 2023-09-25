4f9a63dafdbc58f92e3f6894e77e36bd142b8bb9
dianageorgieva2 <32527999+dianageorgieva2@users.noreply.github.com>
2023-09-25 13:17:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import plotly.io as pio
 import plotly.subplots as sp
 from data_clean import stats_2020_clean, stats_2021_clean, stats_2022_clean, stats_2023_clean, df_statistika_combined, \
     klasirane_2023_combined, yticks_text2
+import textwrap
+from datetime import datetime
 
 pio.templates.default = "plotly"
 pd.set_option('display.max_columns', None)
@@ -16,10 +18,20 @@ pd.set_option('display.width', 1000)
 pd.options.display.float_format = '{:,.2f}'.format
 
 # Dashboard layout
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title='НВО', page_icon="favicon.ico")
 st.markdown("<h1 style='text-align: center;'>Национално Външно Оценяване (НВО)<br><br></h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Здравейте, aз съм Диана.<br>Преминавайки изпитанието на матурите(НВО) след 7ми клас с моя син, "
+            "събрахме всички важни данни публикувани от МОН по темата. За мен беше много полезно да видя цялата "
+            "информация систематизирана и визуалиирана на едно място, за да вземем в процеса на кандидастване възможно"
+            " най-информирано и базирано на данни решение. <br>"
+            "Радвам се да споделим тези данни и се надявам те да помогнат и на други. Тук ще намерите НВО резултатите - 2023 година, средния успех и броя на учениците, успеваемост, най-предпочитани "
+            "и паралелки с минимален или никакъв интерес, свободни места за различните класирания, трендове и повече. "
+            "Пишете ми в коментарите ако искате да добавим още нещо или имате въпроси.<br>"
+            "УСПЕХ НА ВСИЧКИ!</p>", unsafe_allow_html=True)
+st.write("Използван сайт на МОН: [ЛИНК](https://ruo-sofia-grad.com/%D0%B8%D0%B7%D0%BF%D0%B8%D1%82%D0%B8-%D0%B8-%D0%BF%D1%80%D0%B8%D0%B5%D0%BC-%D0%BD%D0%B0-%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%86%D0%B8/%D0%BF%D1%80%D0%B8%D0%B5%D0%BC-%D0%BD%D0%B0-%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%86%D0%B8/%D0%BF%D1%80%D0%B8%D0%B5%D0%BC-%D0%B2-viii-%D0%BA%D0%BB%D0%B0%D1%81/)")
+
+
 col1, col2 = st.columns(2, gap='medium')
-c = st.container()
 
 # Data visualization fig1
 with col2:
@@ -112,6 +124,8 @@ with col1:
                        cliponaxis=False,
                        textfont=dict(size=12),
                        hoverinfo=None)
+    # bar_trace.update(textfont_color=bar_trace.marker.color)
+
     scatter_trace = go.Scatter(x=df_statistika_combined["Година"],
                                y=df_statistika_combined[avg_tochki],
                                name="Среден успех(точки)",
@@ -121,10 +135,12 @@ with col1:
                                textposition="top center",
                                cliponaxis=False,
                                textfont=dict(
-                                   size=12,
+                                   size=14,
                                    color="rgb(239, 85, 59)",
                                ),
                                hoverinfo=None)
+    # scatter_trace.update(textfont_color=scatter_trace.marker.color)
+    # fig2.update_traces(textfont_color=dict(type='markers'))
     fig2.add_trace(bar_trace)
     fig2.add_trace(scatter_trace)
     fig2.update_traces(hoverinfo='none')
@@ -154,10 +170,15 @@ with col1:
 
     st.plotly_chart(fig2, use_container_width=True, config={'displayModeBar': False})
 
+st.divider()
+
 #----------KLASIRANE----------
 # Create fig3
+c = st.container()
 with c:
     st.markdown("<h3 style='text-align: center;'>Класиране 2023 - детайли</h3>", unsafe_allow_html=True)
+    multiselect = st.multiselect("Ако искаш да филтрираш по определени училища, избери от списъка:", options=klasirane_2023_combined['Училище'].drop_duplicates().sort_values(), placeholder="Избери училище/а")
+    df_multiselect = klasirane_2023_combined[klasirane_2023_combined["Училище"].isin(multiselect)]
 
     selected_option = st.radio("Избери опция:", ('Ученици(общо)', 'Ученици(младежи)', 'Ученици(девойки)'), key="fig3_radio",
                                horizontal=True)
@@ -171,115 +192,240 @@ with c:
         x_column = 'Мин_бал_о'
         x2_column = "Места_общ_брой"
 
-    fig3 = sp.make_subplots(rows=1, cols=2, shared_xaxes=True, vertical_spacing=0.05, column_widths=[0.85, 0.10])
-    code_to_uchilishte_map = dict(klasirane_2023_combined[['Код паралелка', 'Училище']].drop_duplicates().values)
-    code_to_paral_map = dict(zip(klasirane_2023_combined['Код паралелка'], klasirane_2023_combined['Паралелка']))
+    if df_multiselect.empty:
+        fig3 = sp.make_subplots(rows=1, cols=2, shared_xaxes=True, vertical_spacing=0.20, column_widths=[0.65, 0.15])
+        code_to_uchilishte_map = dict(klasirane_2023_combined[['Код паралелка', 'Училище']].drop_duplicates().values)
+        code_to_paral_map = dict(zip(klasirane_2023_combined['Код паралелка'], klasirane_2023_combined['Паралелка']))
 
-    for k in klasirane_2023_combined[klasirane_2023_combined['Класиране'] != 5]['Класиране'].unique():
-        df_k = klasirane_2023_combined[klasirane_2023_combined['Класиране'] == k]
-        hover_text = [f"{code} {code_to_paral_map[code]}<br>{code_to_uchilishte_map[code]}"
-            for code in df_k['Код паралелка'].unique()]
-        bars = go.Bar(x=df_k[x_column],
+        for k in klasirane_2023_combined[klasirane_2023_combined['Класиране'] != 5]['Класиране'].unique():
+            df_k = klasirane_2023_combined[klasirane_2023_combined['Класиране'] == k]
+            # hover_text = [f"{code} {code_to_paral_map[code]}<br>{code_to_uchilishte_map[code]}"
+            #     for code in df_k['Код паралелка'].unique()]
+            hover_text = [f"{code} {'<br>'.join(textwrap.wrap(code_to_paral_map[code], width=45))}<br>{'<br>'.join(textwrap.wrap(code_to_uchilishte_map[code], width=45))}"
+                for code in df_k['Код паралелка'].unique()]
+
+            bars = go.Bar(x=df_k[x_column],
+                                   y=df_k['Код паралелка'],
+                                   name=f'{k} класиране',
+                                   text=df_k[x_column],
+                                   textposition="outside",
+                                   cliponaxis=False,
+                                   textfont=dict(size=12),
+                                    hoverlabel=dict(namelength=-1),
+                                   hovertext=hover_text,
+                                   hovertemplate='%{hovertext}<br>Мин. бал: %{x}',
+                                   orientation="h")
+            fig3.add_trace(bars, row=1, col=1)
+
+        placeholder = 550
+        for k in klasirane_2023_combined[klasirane_2023_combined['Класиране'] >= 3]['Класиране'].unique():
+            df_k = klasirane_2023_combined[klasirane_2023_combined['Класиране'] == k]
+            mesta = go.Scatter(x=np.full(df_k.shape[0], f'    за {k}кл.'),
                                y=df_k['Код паралелка'],
-                               name=f'{k} класиране',
-                               text=df_k[x_column],
-                               textposition="outside",
-                               cliponaxis=False,
-                               textfont=dict(size=12),
-                                hoverinfo='none',
-                               hovertext=hover_text,
-                               hovertemplate='%{hovertext}<br>Мин. бал: %{x}',
-                               orientation="h")
-        fig3.add_trace(bars, row=1, col=1)
+                               text=df_k[x2_column],
+                               mode='text',
+                               textfont=dict(size=10),
+                               showlegend=False,
+                               hoverinfo='none',
+                               )
+            placeholder += 30
+            fig3.add_trace(mesta, row=1, col=2)
 
-    placeholder = 550
-    for k in klasirane_2023_combined[klasirane_2023_combined['Класиране'] >= 3]['Класиране'].unique():
-        df_k = klasirane_2023_combined[klasirane_2023_combined['Класиране'] == k]
-        mesta = go.Scatter(x=np.full(df_k.shape[0], f'    за {k}кл.'),
-                           y=df_k['Код паралелка'],
-                           text=df_k[x2_column],
-                           mode='text',
-                           textfont=dict(size=10),
-                           showlegend=False,
-                           hoverinfo='none',
-                           )
-        placeholder += 30
-        fig3.add_trace(mesta, row=1, col=2)
+        fig3.update_layout(
+            hoverlabel_align='left',
+            height=len(klasirane_2023_combined) * 12,
+            plot_bgcolor='white',
+            dragmode=False,
+            margin=dict(l=5, r=5),
+            legend=dict(orientation="v",
+                        yanchor="bottom",
+                        y=1.0001,
+                        xanchor='left',
+                        x=0),
+            xaxis=dict(title='Минимален бал',
+                       titlefont_size=12,
+                       side="top",
+                       showticklabels=False,
+                       showgrid=False,
+                       domain=[0, 0.65],
+                       ),
+            xaxis2=dict(title='Свободни места',
+                       titlefont_size=12,
+                       side="top",
+                       showticklabels=True,
+                       showgrid=False,
+                        type='category',
+                        tickangle=-90,
+                        domain=[0.85, 1],
+                        ),
+            yaxis=dict(showticklabels=True,
+                        type='category',
+                        showgrid=False,
+                        tickmode='array',
+                        side='left',
+                        ticktext=yticks_text2,
+                        range=[len(klasirane_2023_combined['Код паралелка'].unique()) - .5, -.5],
+                        tickvals=klasirane_2023_combined['Код паралелка'].unique(),
+                        position=0,
+                       ),
+            yaxis2=dict(showticklabels=False,
+                       type='category',
+                       showgrid=False,
+                       tickmode='array',
+                       side='right',
+                       ticktext=yticks_text2,
+                       range=[len(klasirane_2023_combined['Код паралелка'].unique()) - .5, -.5],
+                       tickvals=klasirane_2023_combined['Код паралелка'].unique(),
+                       position=0,
+                       )
+        )
+        config = {
+            'scrollZoom': False,
+            'displayModeBar': False,
+            'responsive': True,
+            'showTips': True,
+        }
 
-    # custom_width = 1200
-    # yticks_text_long = [f"{code}<br>{code_to_paral_map[code][:45]}<br>{code_to_uchilishte_map[code][:45]}..."
-    #     for code in klasirane_2023_combined['Код паралелка'].unique()]
-    # yticks_text_short = klasirane_2023_combined['Код паралелка'].unique()
-    #
-    # if custom_width <= 70:
-    #     yticks_text = yticks_text_short
-    # else:
-    #     yticks_text = yticks_text_long
+        buffer = io.StringIO()
+        fig3.write_html(buffer, full_html=True, include_plotlyjs=True, config=config)
+        components.html(buffer.getvalue(), width=None, height=600, scrolling=True)
+    else:
+        fig3 = sp.make_subplots(rows=1, cols=2, shared_xaxes=True, vertical_spacing=0.20, column_widths=[0.65, 0.15])
+        code_to_uchilishte_map = dict(df_multiselect[['Код паралелка', 'Училище']].drop_duplicates().values)
+        code_to_paral_map = dict(zip(df_multiselect['Код паралелка'], df_multiselect['Паралелка']))
 
-    fig3.update_layout(
-        # autosize=True,
-        # minreducedwidth=100,
-        # paper_bgcolor='yellowgreen',
-        height=len(klasirane_2023_combined) * 12,
-        plot_bgcolor='white',
-        dragmode=False,
-        # hovermode='y unified',
-        margin=dict(l=5, r=5),
-        legend=dict(orientation="v",
-                    yanchor="bottom",
-                    y=1.0001,
-                    xanchor='left',
-                    x=0),
-        xaxis=dict(title='Минимален бал',
-                   titlefont_size=12,
-                   # automargin=True,
-                   side="top",
-                   showticklabels=False,
-                   showgrid=False,
-                   domain=[0, 0.85],
-                   ),
-        xaxis2=dict(title='Свободни места',
-                   titlefont_size=12,
-                   automargin=True,
-                   side="top",
-                   showticklabels=True,
-                   showgrid=False,
-                    type='category',
-                    tickangle=-90,
-                    domain=[0.9, 1],
-                    ),
-        yaxis=dict(showticklabels=True,
-                    automargin=True,
-                    type='category',
-                    showgrid=False,
-                    tickmode='array',
-                    side='left',
-                    ticktext=yticks_text2,
-                    range=[len(klasirane_2023_combined['Код паралелка'].unique()) - .5, -.5],
-                    tickvals=klasirane_2023_combined['Код паралелка'].unique(),
-                    position=0,
-                   ),
-        yaxis2=dict(showticklabels=False,
-                   automargin=True,
-                   type='category',
-                   showgrid=False,
-                   tickmode='array',
-                   side='right',
-                   ticktext=yticks_text2,
-                   range=[len(klasirane_2023_combined['Код паралелка'].unique()) - .5, -.5],
-                   tickvals=klasirane_2023_combined['Код паралелка'].unique(),
-                   position=0,
-                   )
-    )
+        for k in df_multiselect[df_multiselect['Класиране'] != 5]['Класиране'].unique():
+            df_k = df_multiselect[df_multiselect['Класиране'] == k]
+            hover_text = [
+                f"{code} {'<br>'.join(textwrap.wrap(code_to_paral_map[code], width=45))}<br>{'<br>'.join(textwrap.wrap(code_to_uchilishte_map[code], width=45))}"
+                for code in df_k['Код паралелка'].unique()]
 
-    config = {
-        'scrollZoom': False,
-        'displayModeBar': False,
-        'responsive': True,
-        'showTips': True,
-    }
+            bars = go.Bar(x=df_k[x_column],
+                          y=df_k['Код паралелка'],
+                          name=f'{k} класиране',
+                          text=df_k[x_column],
+                          textposition="outside",
+                          cliponaxis=False,
+                          textfont=dict(size=12),
+                          hoverlabel=dict(namelength=-1),
+                          hovertext=hover_text,
+                          hovertemplate='%{hovertext}<br>Мин. бал: %{x}',
+                          orientation="h")
+            fig3.add_trace(bars, row=1, col=1)
 
-    buffer = io.StringIO()
-    fig3.write_html(buffer, full_html=True, include_plotlyjs=True, config=config)
-    components.html(buffer.getvalue(), width=None, height=600, scrolling=True)
+        placeholder = 550
+        for k in df_multiselect[df_multiselect['Класиране'] >= 3]['Класиране'].unique():
+            df_k = df_multiselect[df_multiselect['Класиране'] == k]
+            mesta = go.Scatter(x=np.full(df_k.shape[0], f'    за {k}кл.'),
+                               y=df_k['Код паралелка'],
+                               text=df_k[x2_column],
+                               mode='text',
+                               textfont=dict(size=10),
+                               showlegend=False,
+                               hoverinfo='none',
+                               )
+            placeholder += 30
+            fig3.add_trace(mesta, row=1, col=2)
+        yticks_text2_multiselect = [
+            f"{code}-{'<br>'.join(textwrap.wrap(code_to_paral_map[code], width=25))}"
+            for code in df_multiselect['Код паралелка'].unique()
+        ]
+        fig3.update_layout(
+            hoverlabel_align='left',
+            height=len(df_multiselect) * 12 + 180,
+            # plot_bgcolor='F6F4EB',
+            # paper_bgcolor='F6F4EB',
+            dragmode=False,
+            margin=dict(l=5, r=5),
+            legend=dict(orientation="v",
+                        yanchor="bottom",
+                        y=1.0001,
+                        xanchor='left',
+                        x=0),
+            xaxis=dict(title='Минимален бал',
+                       titlefont_size=12,
+                       side="top",
+                       showticklabels=False,
+                       showgrid=False,
+                       domain=[0, 0.65],
+                       ),
+            xaxis2=dict(title='Свободни места',
+                        titlefont_size=12,
+                        side="top",
+                        showticklabels=True,
+                        showgrid=False,
+                        type='category',
+                        tickangle=-90,
+                        domain=[0.85, 1],
+                        ),
+            yaxis=dict(showticklabels=True,
+                       type='category',
+                       showgrid=False,
+                       tickmode='array',
+                       side='left',
+                       ticktext=yticks_text2_multiselect,
+                       range=[len(df_multiselect['Код паралелка'].unique()) - .5, -.5],
+                       tickvals=df_multiselect['Код паралелка'].unique(),
+                       position=0,
+                       ),
+            yaxis2=dict(showticklabels=False,
+                        type='category',
+                        showgrid=False,
+                        tickmode='array',
+                        side='right',
+                        ticktext=yticks_text2_multiselect,
+                        range=[len(df_multiselect['Код паралелка'].unique()) - .5, -.5],
+                        tickvals=df_multiselect['Код паралелка'].unique(),
+                        position=0,
+                        )
+        )
+        config = {
+            'scrollZoom': False,
+            'displayModeBar': False,
+            'responsive': True,
+            'showTips': True,
+        }
 
+        buffer = io.StringIO()
+        fig3.write_html(buffer, full_html=True, include_plotlyjs=True, config=config)
+        components.html(buffer.getvalue(), width=None, height=600, scrolling=True)
+
+
+# Message functionality and history
+def load_msg_history():
+    try:
+        msg_history = pd.read_csv("msg_history.csv")
+    except FileNotFoundError:
+        msg_history = pd.DataFrame(columns=["name", "text", "time"])
+    return msg_history
+
+
+def save_msg_history(msg_history):
+    msg_history.to_csv("msg_history.csv", index=False)
+
+
+msg_history = load_msg_history()
+
+st.divider()
+c2 = st.container()
+c2.markdown("<h3 style='text-align: center;'>Коментари</h3>", unsafe_allow_html=True)
+
+with st.form(key='form1', clear_on_submit=True):
+    user_name = st.text_input("Име")
+    user_message = st.text_area("Съобщение")
+    submit_button = st.form_submit_button("Изпрати")
+    if submit_button:
+        if user_name and user_message:
+            timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            message = {
+                "name": user_name,
+                "text": user_message,
+                "time": timestamp
+            }
+            msg_history = pd.concat([msg_history, pd.DataFrame([message])])
+            save_msg_history(msg_history)
+
+for _, message in msg_history.iterrows():
+    messages = c2.info(f"{message['name']} ({message['time']}) \n{message['text']}")
+
+# html(messages, width=None, height=100, scrolling=True)
