95e6f2cd548c6cc746886c1165aed00efa9950a7
dianageorgieva2 <32527999+dianageorgieva2@users.noreply.github.com>
2023-10-23 14:21:54 +0300

    Add files via upload

---
+++ msg_history.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from pymongo import MongoClient
+
+
+# Connect with Mongo DB
+def db_connection():
+    return MongoClient(st.secrets['db_connect'])
+
+
+client = db_connection()
+
+
+@st.cache_data(ttl=300)
+def get_message_history():
+    db = client.HBO_test
+    records = db.messages.find()
+    records = list(records)
+    return records
+
+
+def create_message(msg):
+    db = client.HBO_test
+    records = db.messages
+    new_record = records.insert_one(msg)
+    return new_record
